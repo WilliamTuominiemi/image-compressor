6406dcdbc3cba921216d79345b42d625189d9acd
Tuominiemi William <william.tuominiemi@aalto.ficd>
2026-04-23 22:31:15 +0300

    list all pixels

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use image::{ImageError, ImageReader};
+use image::{GenericImageView, ImageError, ImageReader};
 
 fn main() -> Result<(), ImageError> {
     let img = ImageReader::open("photo.jpeg")?.decode()?;
     println!("Dimensions: {} x {}", img.width(), img.height());
+
+    for (x, y, pixel) in img.pixels() {
+        let rgba = pixel.0;
+        println!(
+            "Pixel at ({}, {}): Red={}, Green={}, Blue={}, Alpha={}",
+            x, y, rgba[0], rgba[1], rgba[2], rgba[3]
+        );
+    }
+
     Ok(())
 }
