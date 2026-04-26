37ae8b42f670730f05269abc50f24e498f81569f
Tuominiemi William <william.tuominiemi@aalto.ficd>
2026-04-26 18:49:27 +0300

    rgb8 instead of rgb16

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use image::{ImageError, ImageReader};
 
 fn main() -> Result<(), ImageError> {
     let img = ImageReader::open("photo.jpeg")?.decode()?;
-    let mut rgb = img.into_rgb16();
+    let mut rgb = img.into_rgb8();
 
     for (x, y, pixel) in rgb.enumerate_pixels_mut() {
         pixel.0[2] = 0;
-        pixel.0[0] = pixel.0[0].saturating_add(5000);
+        pixel.0[0] = pixel.0[0].saturating_add(50);
     }
 
-    rgb.save("altered_photo.png")?;
+    rgb.save("altered_photo.jpg")?;
 
     Ok(())
 }
